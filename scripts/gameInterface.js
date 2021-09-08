37e0a31c8af5374a4ed844a03a8b14e62d133c8b
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2021-09-08 11:37:39 -0300

    FEATURE: Diagonal bar for the winner and it animation.  STYLE: Animation for "go back button".  CHANGED: All lenghts in css are relative to other.
    FEATURE: Diagonal bar for the winner and it animation.
    
    STYLE: Animation for "go back button".
    
    CHANGED: All lenghts in css are relative to other.

--- scripts/gameInterface.js
+++ scripts/gameInterface.js
@@ -26,9 +26,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let url = new URL(urlString);
     let chosenPlayer = url.searchParams.get("chosenPlayer");
 
+    // Get the player turn
     playerTurn = symbols.indexOf(chosenPlayer);
     updateTurnSpan();
 
+    // Add event listener for each clickable place (square)
     let squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         square.addEventListener('click', click);
@@ -36,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
-
+// When player click in square
 let click = (event) => {
     if (removeAnimation) {
         boardHorizontalLines.forEach((boardHorizontalLine) => {
@@ -48,10 +50,11 @@ let click = (event) => {
     }
     let square = event.target;
     let position = square.id;
+    // How has content inside square, the follow code is for execute a action only if the square is clicked
     if (square.className == "square") {
 
         if (!hasWinner) {
-            // I can access methods from other JS script because this other script was defined firstly then this one.
+            //* I can access methods from other JS script because this other script was defined firstly than this one.
             if (canFillIn(position)) {
                 fillIn(position);
                 updateSquare(square);
@@ -61,30 +64,65 @@ let click = (event) => {
     }
 }
 
+// Insert a value in html for to draw the player on screen
 let updateSquare = (square) => {
     square.innerHTML += playersCode[playerTurn];
 }
 
+// Update the text at the top of the screen that shows the player's turn
 let updateTurnSpan = (position) => {
-    let boardDiv = document.querySelector(".board");
 
     if (!hasWinner) {
         turnSpan.innerHTML = "Turn: " + symbols[playerTurn];
 
-        let win = document.querySelector("#winner_bar_1");
-        // win.classList.add("winner-vertical-line");
-
-        let ver = document.querySelector("#winner_bar_Y_1");
-        //ver.classList.add("winner-horizontal-line");
-
+        if (draw) {
+            turnSpan.innerHTML = "Draw";
+            fadeScreen();
+        }
 
     } else if (hasWinner) {
         turnSpan.innerHTML = "Winner: " + board[position];
-        boardDiv.innerHTML += ""
+        animateWinningSequence();
+    }
+}
+
+let fadeScreen = () => {
+    document.body.classList.add("fade");
+}
+
+// Put a horizontal, vertical or diagonal line 
+let animateWinningSequence = () => {
+    // Making function to check if tow lists are equal
+    function isEqualTo(array) {
+        for (const pos in array) {
+            if (this[pos] !== array[pos]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    winningSequence.isEqualTo = isEqualTo;
+
+    let win = document.getElementById(winningSequence[1]);
+
+    // Choose a color for line
+    let colorClass;
+    if (playerTurn == 0) {
+        colorClass = "white";
+    } else if (playerTurn == 1) {
+        colorClass = "black";
     }
 
-    if (draw) {
-        turnSpan.innerHTML = "Draw";
+    // Choose a square to insert the line
+    if (winningSequence.isEqualTo([0, 1, 2]) || winningSequence.isEqualTo([3, 4, 5]) || winningSequence.isEqualTo([6, 7, 8])) {
+        win.innerHTML += "<div class='winner-horizontal-line " + colorClass + "'></div>";
+    } else if (winningSequence.isEqualTo([0, 3, 6]) || winningSequence.isEqualTo([1, 4, 7]) || winningSequence.isEqualTo([2, 5, 8])) {
+        win.innerHTML += "<div class='winner-vertical-line " + colorClass + "'></div>";
+    } else if (winningSequence.isEqualTo([0, 4, 8])) {
+        win.innerHTML += "<div class='winner-diagonal-line " + colorClass + "'></div>";
+    } else if (winningSequence.isEqualTo([2, 4, 6])) {
+        win.innerHTML += "<div class='winner-diagonal-line " + colorClass + "'></div>";
+        document.querySelector(".winner-diagonal-line").style.transform = ("translate(-50%, -50%) rotate(45deg)")
     }
 
 }
